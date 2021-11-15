9453438cab83e23b5871f4f244af30dfd7010803
Stefan <stefan.deak@computerrock.com>
2021-11-15 14:59:55 +0100

    Updates

---
+++ precache-manifest.5017cda37c4987bada9519b3b77b4bdc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c23e40751dfe6f7e55a7a253e4e3c945",
+    "url": "/grid-battle/index.html"
+  },
+  {
+    "revision": "ee3469d7f5b7cbfed15c",
+    "url": "/grid-battle/static/css/main.4bf48267.chunk.css"
+  },
+  {
+    "revision": "b24fd133d2e256910c28",
+    "url": "/grid-battle/static/js/2.0b38ff1e.chunk.js"
+  },
+  {
+    "revision": "ee3469d7f5b7cbfed15c",
+    "url": "/grid-battle/static/js/main.3f8a8c95.chunk.js"
+  },
+  {
+    "revision": "2f950e24afc65990a19c",
+    "url": "/grid-battle/static/js/runtime-main.5026a2c7.js"
+  },
+  {
+    "revision": "5d77e2a2a9d4e71a649b462ee9c56db8",
+    "url": "/grid-battle/static/media/Lora-Bold.5d77e2a2.woff"
+  },
+  {
+    "revision": "2fdac4a2d128779e77035bd6b9f29283",
+    "url": "/grid-battle/static/media/fortuna.2fdac4a2.png"
+  },
+  {
+    "revision": "c800bdd9e1d2efd9022d54f509414ae3",
+    "url": "/grid-battle/static/media/goblin_regular.c800bdd9.png"
+  },
+  {
+    "revision": "a0b067640034767413801c15831de0cc",
+    "url": "/grid-battle/static/media/golem.a0b06764.png"
+  },
+  {
+    "revision": "a122af343b92185f3ce364b140c40fd6",
+    "url": "/grid-battle/static/media/ground.a122af34.jpg"
+  },
+  {
+    "revision": "f7e5874096a8733089af06a46baf45c3",
+    "url": "/grid-battle/static/media/tree.f7e58740.png"
+  },
+  {
+    "revision": "d8b023ebe22f4b60498ffc66aebc4203",
+    "url": "/grid-battle/static/media/warrior.d8b023eb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2922d0215fbb1ed30a69a1979af4041.js"
+  "/grid-battle/precache-manifest.5017cda37c4987bada9519b3b77b4bdc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/grid-battle/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
